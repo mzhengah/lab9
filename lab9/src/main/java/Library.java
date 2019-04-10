cc122da13c845712778e8536abd9cbc67a014836
mzhengah <mzhengah@csl4wk64.win2k.cs.ust.hk>
2019-04-11 07:46:23 +0800

    I delete "When" in library

--- lab9/src/main/java/Library.java
+++ lab9/src/main/java/Library.java
@@ -3,7 +3,7 @@
  */
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("When there is a fire, commit and push");
+		System.out.println("there is a fire, commit and push");
 	}
     public boolean someLibraryMethod() {
         return true;
